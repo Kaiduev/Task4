2f8402fb53488cf92079b6b0d7c9e0040215c1b5
Kaiduev <baktygul.kaiduev@gmail.com>
2020-03-04 19:14:30 -0500

    Third commit

--- course_app/serializers.py
+++ course_app/serializers.py
@@ -11,3 +11,21 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Course.objects.create(**validated_data)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'imgpath']
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ['id', 'latitude', 'longitude', 'address']
+
+    def create(self, validated_data):
+        return  Branch.objects.create(**validated_data)
--- course_app/views.py
+++ course_app/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Course
+from .models import Course, Category, Branch
 from . import serializers
 from rest_framework import status
 
@@ -33,8 +33,103 @@ class CourseDetail(APIView):
         serializer = serializers.CourseSerializer(course)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        course = Course.objects.get(pk=pk)
+        serializer = serializers.CourseSerializer(course, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk):
         Resp = Course.objects.get(pk=pk)
         course = Course.objects.get(pk=pk)
         course.delete()
         return Response({"success": "Course '{}' was deleted".format(Resp.name)}, status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryView(APIView):
+
+    serializer_class = serializers.CategorySerializer
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = serializers.CategorySerializer(categories, many=True)
+        return Response({"Categories": serializer.data})
+
+    def post(self,request):
+        serializer = serializers.CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            saved_data = serializer.save()
+            return Response({"success": "Category '{}' created successfully".format(saved_data.name)})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoryDetail(APIView):
+
+    serializer_class = serializers.CategorySerializer
+
+    def get(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        serializer = serializers.CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self,request,pk, format=None):
+        categories = Category.objects.get(pk=pk)
+        serializer = serializers.CategorySerializer(categories, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        Resp = Category.objects.get(pk=pk)
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response({"success": "Category '{}' was deleted".format(Resp.name)}, status=status.HTTP_204_NO_CONTENT)
+
+
+class BranchView(APIView):
+
+    serializer_class = serializers.BranchSerializer
+
+    def get(self, request):
+        branches = Branch.objects.all()
+        serializer = serializers.BranchSerializer(branches, many=True)
+        return Response({"Branches": serializer.data})
+
+    def post(self, request):
+        serializer = serializers.BranchSerializer(data=request.data)
+        if serializer.is_valid():
+            saved_data = serializer.save()
+            return Response({"success": "Branch '{}' created successfully".format(saved_data.address)})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class BranchDetail(APIView):
+
+    serializer_class = serializers.BranchSerializer
+
+    def get(self,request, pk):
+        branch = Branch.objects.get(pk=pk)
+        serializer = serializers.BranchSerializer(branch)
+        return Response(serializer.data)
+
+    def put(self,request,pk, format=None):
+        branch = Branch.objects.get(pk=pk)
+        serializer = serializers.BranchSerializer(branch, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        Resp = Branch.objects.get(pk=pk)
+        branch = Branch.objects.get(pk=pk)
+        branch.delete()
+        return Response({"success": "Branch '{}' was deleted".format(Resp.address)}, status=status.HTTP_204_NO_CONTENT)
+
