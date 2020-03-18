b5e068b7f530c68e359eefb0314043258752224c
Kaiduev <you@example.com>
2020-03-18 17:12:36 +0600

    fix

--- course_project/settings.py
+++ course_project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jlr(h7)d+%yf9tr^yu80yri_8!c-_=*gz*w^d8frc^gp345=%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
